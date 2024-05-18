126b8e6a3b8a5acf3f899188626c5d8281ba6b0d
Sebastian Rosende Bricka <peperosende1515@gmail.com>
2024-05-18 12:25:57 -0300

    Componente carrito

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProductosListComponent } from './productos-list/productos-list.component';
+import { CarritoListComponent } from './carrito-list/carrito-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProductosListComponent
+    ProductosListComponent,
+    CarritoListComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/carrito-list/carrito-list.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-carrito-list',
+  templateUrl: './carrito-list.component.html',
+  styleUrl: './carrito-list.component.scss'
+})
+export class CarritoListComponent {
+
+}
